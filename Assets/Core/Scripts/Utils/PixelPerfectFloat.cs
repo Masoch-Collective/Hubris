e3cb4ddb771bb7de783b56c13d1620a32940ec83
Jayde Iris Callejas <jcallejasrodriguez@gmail.com>
2026-05-03 15:29:41 -0400

    Documentation improvements to PPF

--- Assets/Core/Scripts/Utils/PixelPerfectFloat.cs
+++ Assets/Core/Scripts/Utils/PixelPerfectFloat.cs
@@ -7,11 +7,15 @@ namespace Utils {
 
     public enum WorldValueModes {
 
-        Units,
+        Float,
         Pixels
 
     }
 
+    /// <summary>
+    /// Custom float wrapper that allows setting its value as a number of pixels in the inspector
+    /// </summary>
+    /// <remarks>My most twelve-billion IQ utility yet! Has a custom PropertyDrawer to neatly show either the pixel int or the direct float, plus an inline dropdown to switch between the two!</remarks>
     [Serializable]
     public class PixelPerfectFloat {
 
@@ -19,11 +23,24 @@ namespace Utils {
 
         [SerializeField]
         private WorldValueModes mode;
+        /// <summary>
+        /// Whether this float is being controlled directly or via the Pixels int
+        /// </summary>
         public WorldValueModes Mode => mode;
         [SerializeField]
         private int pixels;
+        /// <summary>
+        /// Value represented as the number of pixels (null if mode == float)
+        /// </summary>
         public int? Pixels => mode == WorldValueModes.Pixels ? pixels : null;
+        /// <summary>
+        /// Float value (implicit cast available)
+        /// </summary>
         public float value;
+
+        public static implicit operator float(PixelPerfectFloat ppf) {
+            return ppf.value;
+        }
 
     }
 
