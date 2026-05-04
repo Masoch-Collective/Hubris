67509877ac28136a44d09464d6af168b4cfda9aa
Jayde Iris Callejas <jcallejasrodriguez@gmail.com>
2026-05-04 00:07:01 -0400

    - Reorganized operation clusters so order is always head->side->foot - Implemented grounded boolean (true if either foot has collision) - Added rudimentary walk controls to Controller.cs - Created "Reset Raycasts" menu item function (sets all Raycasters to null and calls the parameter update function) - Made head and foot raycast lengths independent - Tweaked vertical collision detection such that if both heads or feet collide, overlap correction will be applied according to whichever one "sunk farther" into the collider, allowing for exceptional ramp behaviour - Implemented horizontal collision!   - Both raycasts originate from the horizontal middle of the object   - Option to apply or not apply vertical overlap correction if both sides overlap (if off, body will slowly fall if fully inside collider, else will apply vertical correction and "float" to the top of the collider

--- Assets/Core/Scripts/Character/Controller.cs
+++ Assets/Core/Scripts/Character/Controller.cs
@@ -19,14 +19,22 @@ namespace Character {
         }
 
         [SerializeField]
-        public float jumpHeight;
+        private float jumpForce = 0.25f;
+        [SerializeField]
+        private float walkForce = 0.01f;
 
         private void Start() {
         }
 
         private void Update() {
             if (Keyboard.current.spaceKey.wasPressedThisFrame)
-                Rigidbody.velocity = Vector3.up * jumpHeight;
+                Rigidbody.velocity = Vector3.up * jumpForce;
+            int walkDir = 0;
+            if (Keyboard.current.aKey.isPressed)
+                walkDir --;
+            if (Keyboard.current.dKey.isPressed)
+                walkDir++;
+            Rigidbody.velocity += Vector2.right * (walkForce * walkDir);
         }
 
     }
--- Assets/Core/Scripts/Character/Rigidbody.cs
+++ Assets/Core/Scripts/Character/Rigidbody.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 using Utils;
 
@@ -9,20 +10,29 @@ namespace Character {
         [SerializeField]
         private LayerMask collisionLayers; // What layers to use for all other collision raycasts (will be combined with groundLayers)
         [SerializeField]
-        private PixelPerfectFloat verticalRaycastGap; // How far apart the downwards raycasts should be from one another
+        private PixelPerfectFloat verticalGap; // How far apart the upwards & downwards raycasts should be from one another
         [SerializeField]
-        private PixelPerfectFloat feetBottom; // How far down raycasts start
+        private PixelPerfectFloat centerHeight; // Vertical raycast start
         [SerializeField]
-        private PixelPerfectFloat headHeight; // How far up raycasts start
+        private PixelPerfectFloat sidesHeight; // Vertical raycast start
         [SerializeField]
-        private PixelPerfectFloat raycastLength; // How far raycasts travel
+        private PixelPerfectFloat headHeight; // How far head raycasts travel
+        [SerializeField]
+        private PixelPerfectFloat sideWidth; // How far apart the side raycasts should be from one another
+        [SerializeField]
+        private PixelPerfectFloat footHeight; // How far head raycasts travel
+        [SerializeField]
+        private bool applyVerticalOverlapCorrectionIfBothSidesCollide; // Isekai-ass variable name. Used to specify if vertical overlap correction should occur if both horizontal Raycasters hit
 
-        private Raycaster _footLeft;
-        private Raycaster _footRight;
         private Raycaster _headLeft;
         private Raycaster _headRight;
+        private Raycaster _sideLeft;
+        private Raycaster _sideRight;
+        private Raycaster _footLeft;
+        private Raycaster _footRight;
 
         public Vector2 velocity;
+        public bool grounded;
         
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start() {
@@ -36,6 +46,8 @@ namespace Character {
 
             Move(ref velocity);
 
+            grounded = _footLeft.LastHit || _footRight.LastHit;
+
         }
         
         /// <summary>
@@ -43,9 +55,48 @@ namespace Character {
         /// </summary>
         /// <param name="amount">Movement to apply to the body</param>
         private void Move(ref Vector2 amount) {
+            
             transform.Translate(amount);
-            UpdateRaycasterHits();
-            transform.Translate(VerticalCollisions(ref amount));
+            
+            // Apply horizontal collision + correction first so vertical overlap correction only occurs iff there is still vertical overlap after correcting for horizontal overlap
+            UpdateHorizontalRaycasterHits();
+            transform.Translate(HorizontalCollisions(ref amount));
+
+            if (!applyVerticalOverlapCorrectionIfBothSidesCollide && _sideLeft.LastHit && _sideRight.LastHit) {
+                // If both sides collide and body is configured not to apply vertical correction in such case, only perform collision check
+                VerticalCollisions(ref amount);
+                transform.Translate(Vector3.up * -amount.y);
+            } else {
+                //Otherwise, perform collision check and apply vertical overlap correction
+                UpdateVerticalRaycasterHits();
+                transform.Translate(VerticalCollisions(ref amount));
+            }
+        }
+
+        /// <summary>
+        /// Resets velocity and calculates overlap correction if a collision is detected horizontally; aborts if collision is detected on both sides
+        /// </summary>
+        /// <param name="amount">Reference amount to move; resets x value on collision</param>
+        /// <returns>Overlap</returns>
+        private Vector2 HorizontalCollisions(ref Vector2 amount) {
+
+            Vector3 correction = Vector3.zero;
+
+            // If both or neither side raycasts hit something, abort horizontal collision
+            if (_sideLeft.LastHit == _sideRight.LastHit)
+                return correction;
+            
+            if (_sideLeft.LastHit) {
+                correction.x = _sideLeft.distance - _sideLeft.LastHit.distance;
+                amount.x = 0;
+            }
+            if (_sideRight.LastHit) {
+                correction.x = -(_sideRight.distance - _sideRight.LastHit.distance);
+                amount.x = 0;
+            }
+
+            return correction;
+
         }
 
         /// <summary>
@@ -61,13 +112,27 @@ namespace Character {
             Raycaster validCast;
             if (direction <= 0) {
                 if (_footLeft.LastHit || _footRight.LastHit) {
-                    validCast = _footLeft.LastHit ? _footLeft : _footRight;
+                    if (_footLeft.LastHit && _footRight.LastHit)
+                        // If both feet collided, apply overlap correction according to whichever one is stepped higher (allows for good ramp behaviour)
+                        if (_footLeft.LastHit.distance < _footRight.LastHit.distance)
+                            validCast = _footLeft;
+                        else 
+                            validCast = _footRight;
+                    else
+                        validCast = _footLeft.LastHit ? _footLeft : _footRight;
                     correction.y = validCast.distance - validCast.LastHit.distance;
                     amount.y = 0;
                 }
             } else {
                 if (_headLeft.LastHit || _headRight.LastHit) {
-                    validCast = _headLeft.LastHit ? _headLeft : _headRight;
+                    if (_headLeft.LastHit && _headRight.LastHit)
+                        // If both feet collided, apply overlap correction according to whichever one is stepped higher (allows for good ramp behaviour)
+                        if (_headLeft.LastHit.distance < _headRight.LastHit.distance)
+                            validCast = _headLeft;
+                        else 
+                            validCast = _headRight;
+                    else
+                        validCast = _headLeft.LastHit ? _headLeft : _footRight;
                     correction.y = -(validCast.distance - validCast.LastHit.distance);
                     amount.y = 0;
                 }
@@ -83,45 +148,82 @@ namespace Character {
         private void UpdateRaycasterParameters() {
             
             // Ensure all Raycasters exist
-            _footLeft   ??= new Raycaster(transform);
-            _footRight  ??= new Raycaster(transform);
             _headLeft   ??= new Raycaster(transform);
             _headRight  ??= new Raycaster(transform);
+            _sideLeft   ??= new Raycaster(transform);
+            _sideRight  ??= new Raycaster(transform);
+            _footLeft   ??= new Raycaster(transform);
+            _footRight  ??= new Raycaster(transform);
 
-            // Ensure all Raycasters have the same masks and distances
-            _footLeft.mask      = _footRight.mask       = _headLeft.mask        = _footRight.mask       = collisionLayers;
-            _footLeft.distance  = _footRight.distance   = _headLeft.distance    = _headRight.distance   = raycastLength;
+            // Ensure all Raycasters have the same masks and origins
+            _headLeft.mask  = 
+            _headRight.mask = 
+            _sideLeft.mask  = 
+            _sideRight.mask = 
+            _footLeft.mask  = 
+            _footRight.mask = collisionLayers;
+            _headLeft.Origin  = 
+            _headRight.Origin = 
+            _sideLeft.Origin  = 
+            _sideRight.Origin = 
+            _footLeft.Origin  = 
+            _footRight.Origin = transform;
+            
+            // Set distances
+            _headLeft.distance  = 
+            _headRight.distance = 
+            headHeight - centerHeight;
+
+            _sideLeft.distance =
+            _sideRight.distance =
+            sideWidth;
+            
+            _footLeft.distance  = 
+            _footRight.distance = 
+            footHeight + centerHeight;
             
             // Set directions
-            _footLeft   .direction = Vector3.down;
-            _footRight  .direction = Vector3.down;
             _headLeft   .direction = Vector3.up;
             _headRight  .direction = Vector3.up;
+            _sideLeft   .direction = Vector3.left;
+            _sideRight  .direction = Vector3.right;
+            _footLeft   .direction = Vector3.down;
+            _footRight  .direction = Vector3.down;
 
             // Set offsets
-            _footLeft.originOffset =
-                Vector3.down * feetBottom +
-                Vector3.left * verticalRaycastGap;
-            _footRight.originOffset =
-                Vector3.down * feetBottom +
-                Vector3.right * verticalRaycastGap;
             _headLeft.originOffset =
-                Vector3.up * headHeight +
-                Vector3.left * verticalRaycastGap;
+                Vector3.up * centerHeight +
+                Vector3.left * verticalGap;
             _headRight.originOffset =
-                Vector3.up * headHeight +
-                Vector3.right * verticalRaycastGap;
+                Vector3.up * centerHeight +
+                Vector3.right * verticalGap;
+            _sideLeft.originOffset  =
+            _sideRight.originOffset =
+                Vector3.up * sidesHeight;
+            _footLeft.originOffset =
+                Vector3.up * centerHeight +
+                Vector3.left * verticalGap;
+            _footRight.originOffset =
+                Vector3.up * centerHeight +
+                Vector3.right * verticalGap;
             
         }
         
         /// <summary>
-        /// Casts all raycasters and updates their LastHit value
+        /// Casts all vertical raycasters (feet and heads) and updates their LastHit value
         /// </summary>
-        private void UpdateRaycasterHits() {
-            _footLeft.Cast();
-            _footRight.Cast();
+        private void UpdateVerticalRaycasterHits() {
             _headLeft.Cast();
             _headRight.Cast();
+            _footLeft.Cast();
+            _footRight.Cast();
+        }
+        /// <summary>
+        /// Casts all horizontal raycasters (sides) and updates their LastHit value
+        /// </summary>
+        private void UpdateHorizontalRaycasterHits(){
+            _sideLeft.Cast();
+            _sideRight.Cast();
         }
         
         #if UNITY_EDITOR
@@ -129,10 +231,26 @@ namespace Character {
         private void OnValidate() => UpdateRaycasterParameters();
 
         private void OnDrawGizmos() {
-            _footLeft   .DrawGizmo(Color.darkGreen, 1f / PixelPerfectFloat.PixelsPerUnit);
-            _footRight  .DrawGizmo(Color.darkGreen, 1f / PixelPerfectFloat.PixelsPerUnit);
-            _headLeft   .DrawGizmo(Color.greenYellow, 1f / PixelPerfectFloat.PixelsPerUnit);
-            _headRight  .DrawGizmo(Color.greenYellow, 1f / PixelPerfectFloat.PixelsPerUnit);
+            _headLeft   .DrawGizmo(Color.greenYellow,   1f / PixelPerfectFloat.PixelsPerUnit);
+            _headRight  .DrawGizmo(Color.greenYellow,   1f / PixelPerfectFloat.PixelsPerUnit);
+            _sideLeft   .DrawGizmo(Color.red,           1f / PixelPerfectFloat.PixelsPerUnit);
+            _sideRight  .DrawGizmo(Color.red,           1f / PixelPerfectFloat.PixelsPerUnit);
+            _footLeft   .DrawGizmo(Color.darkGreen,     1f / PixelPerfectFloat.PixelsPerUnit);
+            _footRight  .DrawGizmo(Color.darkGreen,     1f / PixelPerfectFloat.PixelsPerUnit);
+        }
+
+        [MenuItem("Utilities/Reset All Raycasters")]
+        private static void ResetRaycasters() {
+            foreach (Rigidbody body in FindObjectsByType<Rigidbody>(FindObjectsSortMode.None)) {
+                body._headLeft  = 
+                body._headRight = 
+                body._sideLeft  = 
+                body._sideRight = 
+                body._footLeft  = 
+                body._footRight = 
+                null;
+                body.UpdateRaycasterParameters();
+            }
         }
         
         #endif
