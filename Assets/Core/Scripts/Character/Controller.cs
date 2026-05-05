1dc599dea6d566f21d06acfdb97766b8fc811346
Jayde Iris Callejas <jcallejasrodriguez@gmail.com>
2026-05-05 01:19:28 -0400

    Implemented coyote time in Controller.cs, moved physics into FixedUpdate(), created custom editor to visualize coyote time status in Controller inspector

--- Assets/Core/Scripts/Character/Controller.cs
+++ Assets/Core/Scripts/Character/Controller.cs
@@ -9,7 +9,6 @@ namespace Character {
     public class Controller : MonoBehaviour {
 
         private Rigidbody _rigidbody;
-
         public Rigidbody Rigidbody {
             get {
                 if (!_rigidbody)
@@ -22,19 +21,46 @@ namespace Character {
         private float jumpForce = 0.25f;
         [SerializeField]
         private float walkForce = 0.01f;
+        
+        [HideInInspector]
+        public int coyoteTimeDuration;
+
+        /// <summary>
+        /// Returns true if the lastGroundedTime is less than coyoteTime ago. Set to true to update lastGroundedTime, false to make lastGroundedTime -inf 
+        /// </summary>
+        public bool CanJump {
+            get => FramesSinceLastGrounded <= coyoteTimeDuration;
+            private set => LastGroundedFrame = value ? FrameCount : int.MinValue;
+        }
+
+        public int LastGroundedFrame {get; private set; }
+        public int FramesSinceLastGrounded => LastGroundedFrame == int.MinValue ? int.MaxValue : FrameCount - LastGroundedFrame;
+        public int FrameCount {get; private set; }
 
         private void Start() {
         }
 
         private void Update() {
-            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            if (Keyboard.current.spaceKey.wasPressedThisFrame && CanJump) {
                 Rigidbody.velocity = Vector3.up * jumpForce;
+                CanJump = false;
+            }
+        }
+
+        private void FixedUpdate() {
+            
+            FrameCount++;
+
+            if (Rigidbody.grounded)
+                CanJump = true;
+
             int walkDir = 0;
             if (Keyboard.current.aKey.isPressed)
-                walkDir --;
+                walkDir--;
             if (Keyboard.current.dKey.isPressed)
                 walkDir++;
             Rigidbody.velocity += Vector2.right * (walkForce * walkDir);
+
         }
 
     }
