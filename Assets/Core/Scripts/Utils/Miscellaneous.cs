133641124d94c6863bb02639d5d0cc5b5bbad6ed
Jayde Iris Callejas <jcallejasrodriguez@gmail.com>
2026-05-13 01:03:04 -0400

    Created Miscellaneous.cs static class, which will contain various standalone functions. Created Function to act as an InputAction callback which will filter inputs to only allow the specified gamepad instance to call the given method

---
+++ Assets/Core/Scripts/Utils/Miscellaneous.cs
@@ -0,0 +1,22 @@
+using UnityEngine.InputSystem;
+
+namespace Utils {
+
+    public static class Miscellaneous {
+        
+        public delegate void ActionFunctionDelegate(InputAction.CallbackContext context);
+        /// <summary>
+        /// Middleman for InputAction callbacks, which calls the given <paramref name="function"/> if the gamepad that triggered the Action matches the <paramref name="filter"/> gamepad; or if <paramref name="allowNonGamepads"/> is true and the Action was triggered by anything other than a gamepad.
+        /// </summary>
+        /// <param name="context">Action callback context.</param>
+        /// <param name="function">Function to call.</param>
+        /// <param name="filter">Gamepad instance allowed to call <paramref name="function"/>; aborts if null.</param>
+        /// <param name="allowNonGamepads">Whether to call <paramref name="function"/> if Action was not triggered by a gamepad.</param>
+        public static void GamepadFilter(InputAction.CallbackContext context, ActionFunctionDelegate function, Gamepad filter, bool allowNonGamepads = true) {
+            if ((context.control.device is Gamepad pad && filter != null && pad == filter) || allowNonGamepads)
+                function(context);
+        }
+
+    }
+
+}
