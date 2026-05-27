728df56c90d7f79614140ee976f0cffb9dd0858d
Jayde Iris Callejas <jcallejasrodriguez@gmail.com>
2026-05-26 22:09:50 -0400

    Expanded Singleton script to have more precise error output; fixed issue where prefab resource was not being loaded correctly.

--- Assets/Core/Scripts/Utils/Singleton.cs
+++ Assets/Core/Scripts/Utils/Singleton.cs
@@ -11,10 +11,18 @@ namespace Utils {
             get {
                 if (_instance == null) // If no instance exists, instantiate one
                     try {
-                        _instance = Instantiate(Resources.Load<T>($"Singletons/{nameof(T)}"));
+                        string path = $"Singletons/{typeof(T)}";
+                        GameObject prefab = (GameObject)Resources.Load(path);
+                        if (prefab == null)
+                            throw new Exception($"Resource \"{path}\" could not be loaded.");
+                        T component;
+                        if ((component = prefab.GetComponent<T>()) == null)
+                            throw new Exception($"The {typeof(T)} singleton prefab did not have a {typeof(T)} component attached to the root GameObject.");
+                        Debug.Log($"No instance of {typeof(T)} exists; instantiating one now.");
+                        _instance = Instantiate(component);
                     }
                     catch (Exception e) {
-                        Debug.LogError($"Error instantiating Singleton<{typeof(T)}>. Does the prefab \"Resources/Singletons/{typeof(T)}\" exist?\n{e}");
+                        Debug.LogError($"Error instantiating Singleton<{typeof(T)}>.\n{e}");
                     }
                 return _instance;
             }
